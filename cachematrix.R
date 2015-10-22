df7ba33d21b04aec326c87311a02b2fa38e257e0
msauvayre <max.sauvayre@free.fr>
2015-10-22 13:06:34 +0200

    Commit with my solution of programming assignement 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 ## test hash
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(my_mat = matrix()) {
+      inv_mat <- NULL
+      set <- function(y) {
+            my_mat <<- y
+            inv_mat <<- NULL
+      }
+      get <- function() my_mat
+      setinvert <- function(solve) inv_mat <<- solve
+      getinvert <- function() inv_mat
+      list(set = set, get = get,
+           setinvert = setinvert,
+           getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(inv_mat, ...) {
         ## Return a matrix that is the inverse of 'x'
+      my_mat <- inv_mat$getinvert()
+      if(!is.null(my_mat)) {
+            message("getting invert data")
+            return(my_mat)
+      }
+      data <- inv_mat$get()
+      my_mat <- solve(data, ...)
+      inv_mat$setinvert(my_mat)
+      my_mat
 }
+
+x = rbind(c(1, -1/4), c(-1/4, 1))
+m = makeCacheMatrix(x)
+m$get()
+test <- cacheSolve(m)
+print(test)
+cacheSolve(test)
