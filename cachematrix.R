a65f7835c631d8ae60da9ca91958f4bbc76c04b6
msauvayre <max.sauvayre@free.fr>
2015-10-22 13:19:13 +0200

    Commit with my solution (the good one i hope) of programming assignement 2

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,14 @@
 
 makeCacheMatrix <- function(my_mat = matrix()) {
       inv_mat <- NULL
-      set <- function(y) {
+      # First method : set a matrix
+	  set <- function(y) {
             my_mat <<- y
             inv_mat <<- NULL
       }
+	  # Second method : get the created matrix
       get <- function() my_mat
+	  # Third & fourth method : set & get the inverted matrix previously created
       setinvert <- function(solve) inv_mat <<- solve
       getinvert <- function() inv_mat
       list(set = set, get = get,
@@ -17,14 +20,14 @@ makeCacheMatrix <- function(my_mat = matrix()) {
            getinvert = getinvert)
 }
 
-
 ## This is my solution of CacheSolve, a function who give the invert of a matrix
 ## who was created before with makeCacheMatrix. It check if the inverted matrix
 ## exist, if so, it compute the inverse of this
 
 cacheSolve <- function(inv_mat, ...) {
-        ## Return a matrix that is the inverse of 'inv_mat'
+      ## Return a matrix that is the inverse of 'inv_mat'
       my_mat <- inv_mat$getinvert()
+	  # Test if the inverted matrix previously created exist
       if(!is.null(my_mat)) {
             message("getting invert data")
             return(my_mat)
